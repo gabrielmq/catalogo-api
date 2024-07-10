30b1768b05e8d3fbb744acb8b8c780f04a9bd587
Gabriel Marques de Souza <gabrielmqs04@gmail.com>
2024-07-10 13:26:31 -0300

    add elasticsearch config

--- infrastructure/src/test/java/io/github/gabrielmsouza/catalogo/infrastructure/APITest.java
+++ infrastructure/src/test/java/io/github/gabrielmsouza/catalogo/infrastructure/APITest.java
@@ -1,4 +1,4 @@
-package io.github.gabrielmsouza.catalogo;
+package io.github.gabrielmsouza.catalogo.infrastructure;
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
